f415f15268ed46d9f2e7e21d2b43ecbbae3d99b4
wangmeng <wangmeng@ksjgs.com>
2019-07-02 11:48:59 +0800

    feat: 轮播图

--- App.js
+++ App.js
@@ -1,109 +1,51 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
 
-import { Image, FlatList, StyleSheet, Text, View } from "react-native";
+import Swiper from 'react-native-swiper'
 
-
-var REQUEST_URL =
-  "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
-
-export default class SampleAppMovies extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false
-    };
-    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向会变为空
-    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
-    // this.fetchData = this.fetchData.bind(this);
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
   }
+})
 
-  componentDidMount() {
-    this.fetchData();
-  }
-  
-  fetchData = async () => {
-    try {
-      let response = await fetch(REQUEST_URL);
-      let jsonResponse = await response.json();
-          console.log('jsonResponse', jsonResponse);
-          this.setState({
-            data: this.state.data.concat(jsonResponse.movies),
-            loaded: true
-          });
-    } catch (error) {
-      console.log(error);
-    }
-    
-  }
- 
-
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
+export default class SwiperComponent extends Component {
+  render(){
     return (
-      <FlatList
-        data={this.state.data}
-        renderItem={this.renderMovie}
-        style={styles.list}
-        keyExtractor={item => item.id}
-      />
-    );
-  }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>Loading movies...</Text>
-      </View>
-    );
-  }
-
-  renderMovie({ item }) {
-    // { item }是一种“解构”写法，请阅读ES2015语法的相关文档
-    // item也是FlatList中固定的参数名，请阅读FlatList的相关文档
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: item.posters.thumbnail }}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{item.title}</Text>
-          <Text style={styles.year}>{item.year}</Text>
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
         </View>
-      </View>
-    );
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+    )
   }
 }
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  rightContainer: {
-    flex: 1
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: "center"
-  },
-  year: {
-    textAlign: "center"
-  },
-  thumbnail: {
-    width: 53,
-    height: 81
-  },
-  list: {
-    paddingTop: 20,
-    backgroundColor: "#F5FCFF"
-  }
-});
+AppRegistry.registerComponent('myproject', () => SwiperComponent);
